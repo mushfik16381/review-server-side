512a34af7a8f96e66c4f9f0a38fdb653bb62ba55
mushfik16381 <ahmed.mushfik16381@gmail.com>
2022-11-09 21:11:01 +0600

    services added

--- index.js
+++ index.js
@@ -13,7 +13,25 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.82ifdqx.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const serviceCollection = client.db('squidfood').collection('services')
+
+        app.get('/services', async(req, res) =>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err));
 
 
 
