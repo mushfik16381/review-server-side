a849552662867fe15f0f76d7d6a231427f852392
mushfik16381 <ahmed.mushfik16381@gmail.com>
2022-11-10 10:02:34 +0600

    service id get app

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -23,6 +23,12 @@ async function run(){
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+        app.get('/services/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
         })
     }
     finally{
